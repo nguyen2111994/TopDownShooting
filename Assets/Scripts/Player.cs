2a1802f7edd9eb6aff6ffdb63eb154cbb720638d
anh <nguyen2111994@gmail.com>
2022-12-21 16:21:22 +0700

    upGame

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))] // Tu add them playerController scripts vao. 
+public class Player : MonoBehaviour
+{
+    PlayerController controller;
+    public float moveSpeed = 5f;
+
+    Camera viewCamera;
+
+    private void Start()
+    {
+        controller = GetComponent<PlayerController>();
+        viewCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+        controller.Move(moveVelocity);
+
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition); // Ve ra mot Duong Thang tu Camera den con cho chuot;
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero); // Ve ra mot Mat Phang ;
+        float rayDistance;
+
+        if(groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            Debug.DrawLine(ray.origin, point, Color.red);
+            controller.LookAt(point);
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]   // Tu dong them Rigidbody vao Object;  
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody playerRigidbody;
+    Vector3 velocity;
+
+    private void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void LookAt(Vector3 lookPoint)
+    {
+        Vector3 heightCorrectedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        transform.LookAt(heightCorrectedPoint);
+    }
+
+    public void FixedUpdate()
+    {
+        playerRigidbody.MovePosition(playerRigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
